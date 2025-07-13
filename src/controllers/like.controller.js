6edd810c461057a8ee24d3f9fabdbecee809df61
Karan Kumar <karan2160639@gmail.com>
2025-07-13 17:42:56 +0530

    coded and tested like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,27 +7,71 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+
+    const existedLike=await Like.findOne({
+        video: videoId,
+        likedBy: req.user._id
+    })
+
+    if(existedLike){
+        await Like.deleteOne({
+            video: videoId,
+            likedBy: req.user._id
+        })
+    }else{
+        await Like.create({
+            video: videoId,
+            likedBy: req.user._id 
+        })
+    }
+
+    return res.status(200).json(
+        new ApiResponse(201, {}, existedLike? "video disliked successfully" : "video liked successfully")
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
 
-})
+    const existedLike=await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    })
 
-const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+    if(existedLike){
+        await Like.deleteOne({
+            comment: commentId,
+            likedBy: req.user._id
+        })
+    }else{
+        await Like.create({
+            comment: commentId,
+            likedBy: req.user._id 
+        })
+    }
+
+    return res.status(200).json(
+        new ApiResponse(201, {}, existedLike? "comment disliked successfully" : "comment liked successfully")
+    )
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const allLikedVideos=await Like.find({
+        likedBy: req.user._id,
+        video: {
+            $exists: true
+        }
+    })
+
+    return res.status(200).json(
+        new ApiResponse(201, allLikedVideos, "all liked videos fetched successfully")
+    )
 })
 
 export {
     toggleCommentLike,
-    toggleTweetLike,
     toggleVideoLike,
     getLikedVideos
 }
